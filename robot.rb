82686d99480460632595e712e19ebb14cc5613e3
Gizipp Aprianto <gizipp@softwareseni.com>
2014-09-30 09:01:47 +0700

    Add function move the robot. Change y variable into f

--- robot.rb
+++ robot.rb
@@ -1,18 +1,33 @@
 class Robot
   def initialize(args)
-    #contstruct
+    @x = 0
+    @y = 0
+    @f = 0
   end
 
   private
-  def place(x,y,z)
+  def place(x,y,f)
     @x = x.to_i
     @y = y.to_i
-    @z = z.to_s
+    @f = z.to_s
     print "Robot on the table!\n"
   end
   
   def move
-    # move the robot
+     if @x > 3 || @y > 3
+      print "Do not move, or robot falling from the table!\n"
+      else
+        case @y
+        when "NORTH"
+          @y += 1
+        when "EAST"
+          @x += 1
+        when "SOUTH"
+          @y -= 1
+        when "WEST"
+          @x -= 1
+        end
+      end
   end
 
   def left
