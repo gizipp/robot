3ceed720c9176bf0dfbbbb9f73c7550ae502110d
Gizipp Aprianto <gizipp@softwareseni.com>
2014-09-30 08:42:55 +0700

    Add robot class

---
+++ robot.rb
@@ -0,0 +1,26 @@
+class Robot
+  def initialize(args)
+    #contstruct
+  end
+
+  private
+  def place(x,y,z)
+     # placing the robot
+  end
+  
+  def move
+    # move the robot
+  end
+
+  def left
+    # robot, please turn left
+  end
+
+  def right
+     # robot, please turn right
+  end
+
+  def report
+     # robot, send the status!
+  end
+end
